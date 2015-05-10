d4e50c516e35c900a737b21deca23ee8ebae4156
Scott Moynes <scott.moynes@gmail.com>
2015-05-10 00:13:11 -0700

    Add a create action for jobs

--- web/controllers/job_controller.ex
+++ web/controllers/job_controller.ex
@@ -9,4 +9,18 @@ defmodule GetAJobEx.JobController do
     jobs = Repo.all(Job)
     render conn, jobs: jobs
   end
+
+  def create(conn, %{"job" => job_params}) do
+    changeset = Job.changeset(%Job{}, job_params)
+
+    if changeset.valid? do
+      id = Repo.insert(changeset)
+      redirect conn, to: job_path(conn, :show, id)
+    else
+      conn
+      |> put_status(400)
+      |> text "invalid"
+    end
+  end
+
 end
