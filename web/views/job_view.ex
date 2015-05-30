615afe41ebe26e5915050bbdb42286f52b96574b
Scott Moynes <scott.moynes@gmail.com>
2015-05-30 14:34:51 -0700

    Use path helper to render responses.

--- web/views/job_view.ex
+++ web/views/job_view.ex
@@ -1,18 +1,12 @@
 defmodule GetAJobEx.JobView do
+  import GetAJobEx.Router.Helpers
+
   use GetAJobEx.Web, :view
 
-  def render("api.json", _) do
-    %{job: %{number_one: nil,
-             number_two: nil,
-             status: nil,
-             links: [
-               %{href: "/api/jobs",
-                 rel: "index"}
-             ],
-            }
-     }
+  def render("api.json", %{conn: conn}) do
+    job(conn)
   end
-  
+
   def render("index.json", %{jobs: jobs}) do
     %{data: jobs}
   end
@@ -21,4 +15,15 @@ defmodule GetAJobEx.JobView do
     %{job: job}
   end
 
+  defp job(conn) do
+    %{number_one: nil,
+      number_two: nil,
+      status: nil,
+      links: links(conn)}
+  end
+
+  defp links(conn) do
+    %{href: job_path(conn, :index),
+      rel: "index"}
+  end
 end
